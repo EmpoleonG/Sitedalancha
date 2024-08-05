9f43ec335d11b894b4ecdd2012b5ee336f07f893
Igor Cintra <132596295+EmpoleonG@users.noreply.github.com>
2024-08-04 23:17:45 -0300

    mobile-navbar.js

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,32 +1,8 @@
-class MobileNavbar {
-    constructor(mobileMenu, links, navLinks) {
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.links = document.querySelector(links);
-        this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass = "active";
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburguer = document.querySelector('.hamburguer');
+    const navLink = document.querySelector('.nav-link');
 
-        this.handleClicker = this.handleClicker.bind(this);
-    }
-
-    handleClicker() {
-        console.log(this);
-        this.links.classList.toggle(this.activeClass);
-    }
-
-    addClickEvent() {
-        this.mobileMenu.addEventListener("click", this.handleClicker);
-    }
-
-    init() {
-        if (this.mobileMenu) {
-            this.addClickEvent();
-        }
-        return this;
-    }
-}
-
-const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    "links",
-    "links li",
-);
+    hamburguer.addEventListener('click', () => {
+        navLink.classList.toggle('active');
+    });
+});
